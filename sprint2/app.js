6e1733fe3ab3cde008f4ac623236502e56b3bbc2
Andrea Fernandez <andreafernandez1257@gmail.com>
2021-12-17 19:16:46 -0300

    solo prueba

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,39 +6,42 @@ const app = express();
 const publicPath = path.resolve(__dirname,'./public');
 
 app.use(express.static(publicPath));
-
-app.listen(3030, () => {
-    console.log('El servidor esta funcionando en el puerto 3030');
+app.get('/',(req,res)=>{
+    res.send('hola');
 })
 
-app.get('/', (req, res) => {
+app.listen(3030, () => {
+    console.log('El servidor esta funcionando en el puerto 3000');
+})
+
+app.get('/h', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home1.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/home2', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home2.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/Login', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/Login.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/registro', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/registro.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/descripcion', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/CarritoParte1.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/CarritoParte1', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/CarritoParte2.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/CarritoParte2', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/CarritoParte3.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/CarritoParte4', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/CarritoParte4.html'));
 });
