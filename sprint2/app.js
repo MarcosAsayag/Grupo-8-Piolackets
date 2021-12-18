bf1c565c8d054d70fb3386a0f6fef0947ec4f5d2
MarcosAsayag <marasayag@gmail.com>
2021-12-18 11:26:46 -0300

    Ruteo a HOME y comentario de prueba de server

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,15 +6,15 @@ const app = express();
 const publicPath = path.resolve(__dirname,'./public');
 
 app.use(express.static(publicPath));
-app.get('/',(req,res)=>{
-    res.send('hola');
-})
+// app.get('/',(req,res)=>{
+ //  res.send('hola');// 
+//})//
 
 app.listen(3030, () => {
     console.log('El servidor esta funcionando en el puerto 3000');
 })
 
-app.get('/h', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./views/home1.html'));
 });
 
