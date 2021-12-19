99d4b57baac54537f2ec48b2f34c9e1b8e2e124e
Ghoji-o <94145190+Ghoji-o@users.noreply.github.com>
2021-12-18 23:18:05 -0300

    18/12/2021
    Actualización de home 1 y 2

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,12 +6,12 @@ const app = express();
 const publicPath = path.resolve(__dirname,'./public');
 
 app.use(express.static(publicPath));
-// app.get('/',(req,res)=>{
- //  res.send('hola');// 
-//})//
+/* app.get('/',(req,res)=>{
+   res.send('hola');
+})*/
 
 app.listen(3030, () => {
-    console.log('El servidor esta funcionando en el puerto 3000');
+    console.log('El servidor esta funcionando en el puerto 3030');
 })
 
 app.get('/', (req, res) => {
