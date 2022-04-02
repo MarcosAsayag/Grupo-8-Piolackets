7c35a8ea9cf26891dfb648d92b7f3ba47abd5699
JuanGyldenfeldt <94145723+GyldenfeldtJuan@users.noreply.github.com>
2022-04-02 02:09:38 -0300

    Update userController.js

--- sprint2/controllers/userController.js
+++ sprint2/controllers/userController.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const userController = {
     users: (req,res) => {
-        res.render(path.join(__dirname, '../views/users/registro.ejs'));
+        res.render(path.join(__dirname, '../views/users/user.ejs'));
     }
 };
 
