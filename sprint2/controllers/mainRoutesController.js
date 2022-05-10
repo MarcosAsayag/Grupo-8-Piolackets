0c04949f34f62d14994081177a58630f5f355bb4
Ghoji-o <94145190+Ghoji-o@users.noreply.github.com>
2022-05-10 16:57:29 -0300

    Vistas listas

--- sprint2/controllers/mainRoutesController.js
+++ sprint2/controllers/mainRoutesController.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 const mainRoutesController = {
     home: (req, res) => {
-        res.render(path.join(__dirname,'../views/home1.ejs'))
+        res.render(path.join(__dirname,'../views/home.ejs'))
     },
 
     products: (req, res) => {
-        res.render(path.join(__dirname,'../views/home2.ejs'))
+        res.render(path.join(__dirname,'../views/products.ejs'))
     }
 };
 
